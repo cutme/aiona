1ba6edabdcdbd571f8ee606cb329405c7c0139d1
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2019-06-13 12:07:51 +0200

    Slideshow and map fixes

--- src/js/map.js
+++ src/js/map.js
@@ -19,23 +19,28 @@ document.addEventListener('DOMContentLoaded', function() {
             const map = new googleMaps.Map(el, {
                 center: myLatLng,
                 disableDefaultUI: true,
-                zoom: 14,
+                zoom: 12,
                 styles: [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}]
             })
 
             const image = {
-                url: el.getAttribute('data-marker'),
-                scaledSize: new google.maps.Size(141, 39), // scaled size
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(0, 64)
+                url: el.getAttribute('data-marker')
             }
+            
+            var marker_image = new google.maps.MarkerImage(
+                el.getAttribute('data-marker'),
+                 null,
+                 // The origin for my image is 0,0.
+                 new google.maps.Point(0, 0),
+                 // The center of the image is 50,50 (my image is a circle with 100,100)
+                 new google.maps.Point(70, 43)
+             );
 			
 			const marker = new google.maps.Marker({
 				position: myLatLng,
 				map: map,
 				draggable: false,
-				zIndex: 20,
-				icon: image,
+				icon: marker_image,
 				
 				animation: google.maps.Animation.DROP,				
 			});
